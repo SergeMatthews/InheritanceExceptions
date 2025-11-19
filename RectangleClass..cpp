27df526807f0266eee9ec3cb59a1d4cd4bc3df27
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-19 15:14:54 +0600

    Update RectangleClass..cpp

--- RectangleClass..cpp
+++ RectangleClass..cpp
@@ -1,7 +1,6 @@
 #include "RectangleClass.h"
 
-setlocale(LC_ALL, "RU");
 Rectangle::Rectangle(int longSide_length, int shortSide_length) :
     Parallelogram(longSide_length, shortSide_length, 90, 90) {
-    figureName = "Ïðÿìîóãîëüíèê";
+    figureName = "Прямоугольник";
 }

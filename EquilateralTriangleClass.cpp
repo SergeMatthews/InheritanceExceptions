98f4392aa65d17d73a732d06e567afba43e28ae2
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-25 21:15:51 +0600

    Update EquilateralTriangleClass.cpp

--- EquilateralTriangleClass.cpp
+++ EquilateralTriangleClass.cpp
@@ -1,5 +1,11 @@
 #include "EquilateralTriangleClass.h"
+#include "CreationErrorClass.h"
 
-EquilateralTriangle::EquilateralTriangle(int side_length) : IsoscelesTriangle(side_length, side_length, 60, 60) {
+EquilateralTriangle::EquilateralTriangle(int side_length) : IsoscelesTriangle(side_length, side_length, 60, 60) 
+{
     figureName = "Равносторонний треугольник";
+    if ((angle_A != angle_B) || (angle_B != angle_C))
+        throw creation_error("Ошибка создания фигуры. Углы не равны");
+    if ((length_a != length_b) || (length_b != length_c))
+        throw creation_error("Ошибка создания фигуры. Стороны не равны");
 }

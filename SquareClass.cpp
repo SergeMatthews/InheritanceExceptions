fe4ded4634359c37fc205305a7f8c601f6921c96
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-25 21:19:41 +0600

    Update SquareClass.cpp

--- SquareClass.cpp
+++ SquareClass.cpp
@@ -1,5 +1,10 @@
 #include "SquareClass.h"
+#include "CreationErrorClass.h"
 
 Square::Square(int length) : Rectangle(length, length) {
     figureName = "Квадрат";
+    if ((angle_A != angle_B) || (angle_B != angle_C) || (angle_C != angle_D) || (angle_A != 90))
+        throw creation_error("Ошибка создания фигуры. Углы не равны 90 градусов");
+    if ((length_a != length_b) || (length_b != length_c) || (length_c != length_d))
+        throw creation_error("Ошибка создания фигуры. Стороны попарно не равны");
 }

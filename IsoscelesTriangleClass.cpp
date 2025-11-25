df35e47b99abdd2d40d3de3bca8bae86a3339f99
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-25 21:17:07 +0600

    Update IsoscelesTriangleClass.cpp

--- IsoscelesTriangleClass.cpp
+++ IsoscelesTriangleClass.cpp
@@ -1,6 +1,9 @@
-#include "IsoscelesTriangleClass.h"
-
 IsoscelesTriangle::IsoscelesTriangle(int base_length, int sides_length, int base_angle, int top_angle) :
-    Triangle(sides_length, base_length, sides_length, base_angle, top_angle, base_angle) {
+    Triangle(sides_length, base_length, sides_length, base_angle, top_angle, base_angle) 
+{
     figureName = "Равнобедренный треугольник";
+    if (angle_A != angle_C)
+        throw creation_error("Ошибка создания фигуры. Углы A и C не равны");
+    if (length_a != length_c)
+        throw creation_error("Ошибка создания фигуры. Стороны a и c не равны");
 }

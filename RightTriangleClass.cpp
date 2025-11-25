c648f27829b76f5d6f29998d3b0eb71941c132cc
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-25 21:19:19 +0600

    Update RightTriangleClass.cpp

--- RightTriangleClass.cpp
+++ RightTriangleClass.cpp
@@ -1,7 +1,10 @@
 #include "RightTriangleClass.h"
+#include "CreationErrorClass.h"
 
 RightTriangle::RightTriangle(int length_a, int length_b, int length_c, int angle_A, int  angle_B) :
     Triangle(length_a, length_b, length_c, angle_A, angle_B, 90)
 {
     figureName = "Прямоугольный треугольник";
+    if (angle_C != 90)
+        throw creation_error("Ошибка создания фигуры. Угол C не равен 90 градусов");
 }

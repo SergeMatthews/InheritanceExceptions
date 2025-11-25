a684d6a3b9f8cb118905e6b8306dcd57d0e8fae4
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-11-25 21:18:50 +0600

    Update RhombusClass.cpp

--- RhombusClass.cpp
+++ RhombusClass.cpp
@@ -1,6 +1,12 @@
 #include "RhombusClass.h"
+#include "CreationErrorClass.h"
 
 Rhombus::Rhombus(int side_length, int acute_angle, int obtuse_angle) :
-    Parallelogram(side_length, side_length, acute_angle, obtuse_angle) {
+    Parallelogram(side_length, side_length, acute_angle, obtuse_angle) 
+{
     figureName = "Ромб";
+    if ((angle_A != angle_C) || (angle_B != angle_D))
+        throw creation_error("Ошибка создания фигуры. Углы попарно не равны");
+    if ((length_a != length_b) || (length_b != length_c) || (length_c != length_d))
+        throw creation_error("Ошибка создания фигуры. Стороны не равны");
 }
